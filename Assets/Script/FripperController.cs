04074cfbe7fad94519c05cd964743e8d32589893
KyogoTerashima <kyogoterashima@outlook.jp>
2020-04-24 21:07:18 +0900

    Lesson５課題提出　PinBallゲームの作成

---
+++ Assets/Script/FripperController.cs
@@ -0,0 +1,144 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FripperController : MonoBehaviour {
+
+    //HingeJointのコンポーネントを入れる
+    private HingeJoint myHingeJoint;
+
+    //初期の傾き
+    private float defaultAngle = 20;
+    // 弾いた時の傾き
+    private float flickAngle = -20;
+
+    //タッチクラスのインスタンス作成
+    private Touch touch = new Touch();
+
+
+	// Use this for initialization
+	void Start () {
+        //HingeJointコンポーネントの取得
+        this.myHingeJoint = GetComponent<HingeJoint>();
+
+        //フリッパーの傾きを設定
+        SetAngle(this.defaultAngle);
+
+        
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //左矢印キーを押したとき左フリッパーを動かす
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && tag == "LeftFripperTag")
+        {
+            SetAngle(this.flickAngle);
+        }
+
+        //右矢印キーを押したとき右フリッパーを動かす
+        if (Input.GetKeyDown(KeyCode.RightArrow) && tag == "RightFripperTag")
+        {
+            SetAngle(this.flickAngle);
+        }
+
+        //矢印キーを離した時左フリッパーを元に戻す
+        if (Input.GetKeyUp(KeyCode.LeftArrow) && tag == "LeftFripperTag")
+        {
+            SetAngle(this.defaultAngle);
+        }
+
+        //矢印キーを離した時右フリッパーを元に戻す
+        if (Input.GetKeyUp(KeyCode.RightArrow) && tag == "RightFripperTag")
+        {
+            SetAngle(this.defaultAngle);
+        }
+
+        //タッチしたとき
+     //  if (OnTouchDown())
+     //  {
+     //
+     //      SetAngle(this.flickAngle);
+     //      Debug.Log("タッチされました");
+     //  }
+     //  else
+     //  {
+     //
+     //      SetAngle(this.defaultAngle);
+     //      Debug.Log("タッチされていません。");
+     //  }
+
+        for (int i = 0; i < Input.touchCount; i++)
+        {
+            //タッチ情報をコピー
+            Touch t = Input.GetTouch(i);
+            if (t.phase == TouchPhase.Began)
+            {
+                // タッチしたとき
+                Input.GetTouch(i);
+                // タッチした座標に応じて左右のフリッパーを上下する
+                if(t.position.x < Screen.width / 2.0f)
+                {
+                    if(tag == "LeftFripperTag")
+                    {
+
+                    SetAngle(this.flickAngle);
+                    }
+                }
+                else
+                {
+                    if(tag == "RightFripperTag")
+                    {
+
+                    SetAngle(this.flickAngle);
+                    }
+                }
+            }
+            else if (t.phase == TouchPhase.Ended)
+            {
+                // 離した時
+                SetAngle(this.defaultAngle);
+            }
+
+
+        }
+    }
+
+    public void SetAngle (float angle)
+    {
+        JointSpring jointSpr = this.myHingeJoint.spring;
+        jointSpr.targetPosition = angle;
+        this.myHingeJoint.spring = jointSpr;
+
+    }
+
+    bool OnTouchDown()
+    {
+        //タッチされている時
+        if( 0 < Input.touchCount)
+        {
+            //タッチされている指の数だけ処理
+            for(int i = 0; i < Input.touchCount; i++)
+            {
+                //タッチ情報をコピー
+                Touch t = Input.GetTouch(i);
+                //タッチした位置からRayを飛ばす
+                Ray ray = Camera.main.ScreenPointToRay(t.position);
+                RaycastHit hit = new RaycastHit();
+                if(Physics.Raycast(ray, out hit))
+                {
+                    //Rayを飛ばしてあたったオブジェクトが自分自身だったら
+                    if(hit.collider.gameObject == this.gameObject)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+        }
+        return false;
+    }
+
+}
---
+++ Assets/Script/ScoreController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreController : MonoBehaviour {
+
+    //smallstarのスコア
+    private int smallStarSore;
+    //Largestarのスコア
+    private int largeStarSore;
+    //smallCloudのスコア
+    private int SmallCloudSore;
+    //LargeCloudのスコア
+    private int largeCloudSore;
+    //スコアを表示するテキスト
+    private GameObject scoreText = null;
+    //スコアを入れる変数
+    private int sum = 0;
+
+    // Use this for initialization
+    void Start () {
+        //シーン中のScoreテキストを取得
+        this.scoreText = GameObject.Find("Score");
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Text score_text = this.scoreText.GetComponent<Text>();
+        score_text.text = this.sum.ToString();
+        Debug.Log(sum);
+		
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        string yourTag = collision.gameObject.tag;
+
+        //タグによってスコアを入れる
+        if (yourTag == "SmallStarTag")
+        {
+            this.smallStarSore = 5;
+            this.sum += this.smallStarSore;
+        }
+        else if (yourTag == "LargeStarTag")
+        {
+            this.largeStarSore = 20;
+            this.sum += this.largeStarSore;
+        }
+        else if (yourTag == "SmallCloudTag")
+        {
+            this.SmallCloudSore = 5;
+            this.sum += this.SmallCloudSore;
+        }
+        else if (yourTag == "LargeCloud")
+        {
+            this.largeCloudSore = 10;
+            this.sum += this.largeCloudSore;
+        }
+
+    }
+}
